972d56df356b4a579acb815c61455bedfe25e3d1
themoffat28 <bthemoffat@gmail.com>
2016-09-08 09:25:55 -0600

    Imported Scanner function and created a response back feature to "How was your day?".

--- src/stupid/controller/StupidController.java
+++ src/stupid/controller/StupidController.java
@@ -1,17 +1,30 @@
 package stupid.controller;
 
-public class StupidController 
+import java.util.Scanner; //Telling program to grab instructions of scanners from util folder. 
+
+public class StupidController
 {
 	private String myName;
-	
-	public StupidController() 
+	private Scanner inputReader; // Data members
+
+	public StupidController() //Initalizes a data member (strings)
 	{
 		myName = "Braeden";
+		inputReader = new Scanner(System.in); 
+	}
+
+	public void start() //Method section
+	{
+		System.out.println("My name is " + myName);
+		System.out.println("This program talks back");
+		askQuestions();
+	}
+
+	private void askQuestions()
+	{
+		System.out.println("How was your day?");
+		String input = inputReader.nextLine(); 
+		System.out.println("Oh, you had a " + input + " day?");
 	}
 	
-	public void start()
-	{
-		System.out.println("This program is less stupid");
-		System.out.println("My name is " + myName);
-	}
 }
